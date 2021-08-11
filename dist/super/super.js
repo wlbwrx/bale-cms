78f05730913c0a1365877a3635ba09347752140a
Ryhs <18811021309@163.com>
2021-08-11 15:15:05 +0800

    #

---
+++ dist/super/super.js
@@ -0,0 +1,20 @@
+/*easyui样式初始化*/
+$.fn.tabs.defaults.tabHeight = 36; //tab标签条高度
+$.fn.linkbutton.defaults.height = 36; //按钮高度
+$.fn.menu.defaults.itemHeight = 30; //menu高度
+
+$.fn.validatebox.defaults.height = 36;
+$.fn.textbox.defaults.height = 36;
+$.fn.textbox.defaults.iconWidth = 26;
+$.fn.datebox.defaults.height = 36;
+$.fn.numberspinner.defaults.height = 36;
+$.fn.timespinner.defaults.height = 36;
+$.fn.numberbox.defaults.height = 36;
+$.fn.combobox.defaults.height = 36;
+$.fn.combotree.defaults.height = 36;
+$.fn.combogrid.defaults.height=36;
+$.fn.passwordbox.defaults.height = 36;
+$.fn.filebox.defaults.height = 36;
+
+$.fn.menu.defaults.noline = true
+$.fn.progressbar.defaults.height = 20; //进度条
